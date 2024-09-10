8dca697e741a638396f5c458725c09b2aa9dd66c
Anatoliy Oleynikov <oleinikovav.ao@gmail.com>
2024-09-10 18:01:08 +0300

    task7

--- module_9_7.py
+++ module_9_7.py
@@ -1,6 +1,22 @@
+
+def is_prime(func):
+    def wrapper(*args):
+        result = func(*args)  # Вызов оригинальной функции с параметрами
+        if result <= 1:
+            print('Составное')  # 0 и 1 не являются простыми числами
+            return result
+        else:
+            for i in range(2, int(result**0.5) + 1):
+                if result % i == 0:
+                    print('Составное')
+                    return result
+            print('Простое')
+        return result
+    return wrapper
+
+@is_prime
 def sum_three(a,b,c):
     return a+b+c
 
-def is_prime(a):
-    def  wrapper(func):
-
+result = sum_three(2, 3, 6)
+print(result)
